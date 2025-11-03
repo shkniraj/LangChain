d49e8d6230cdb3e500abf89c9a6f00e7712d9f8c
SHKNIRAJ <sk.niraj3128@gmail.com>
2025-11-03 20:53:13 +0530

    Squashed 'Anthropic/' content from commit 69cf74a
    git-subtree-dir: Anthropic
    git-subtree-split: 69cf74a90915d006ff53eba182a39704936cb56d

---
+++ LangChain_anthropic/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DotNetEnv;
+using LangChain.Providers;
+using LangChain.Providers.Anthropic;
+
+Env.Load();
+
+var apiKey = Environment.GetEnvironmentVariable("ANTHROPIC_API_KEY")
+                     ?? throw new InvalidOperationException("ANTHROPIC_API_KEY not set");
+
+var provider = new AnthropicProvider(apiKey);
+
+var chat = new AnthropicChatModel(provider, "claude-4.5-sonnet");
+
+var reply = await chat.GenerateAsync("Explain Deeplearning");
+Console.WriteLine(reply);
