576bf2f0fea7ee41748a6dd00dcf26d3691c7728
SHKNIRAJ <sk.niraj3128@gmail.com>
2025-11-03 20:52:14 +0530

    Squashed 'Ollama/' content from commit 5decec1
    git-subtree-dir: Ollama
    git-subtree-split: 5decec1ea5ab1f7b05670b96ccb29b84212a13dc

---
+++ LangChain_Ollama/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LangChain.Providers;
+using LangChain.Providers.Ollama;
+using static LangChain.Chains.Chain;
+
+var provider = new OllamaProvider();
+var llm = new OllamaChatModel(provider, id: "phi3:mini").UseConsoleForDebug();
+
+var prompt = """
+explain async/await.
+""";
+
+var chain = Set(prompt) | LLM(llm);
+var answer = await chain.RunAsync("text");
+Console.WriteLine(answer);
+
+Console.WriteLine("Hello, World!");
